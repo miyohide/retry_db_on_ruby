63ff16445d18c81069a455d53bea0a74649f5926
Hidenori Miyoshi <miyohide@gmail.com>
2021-11-02 21:12:55 +0900

    connect db and load models

---
+++ config/init.rb
@@ -0,0 +1,18 @@
+require "active_record"
+require "active_support/time"
+require "erb"
+
+CONFIG_DIR = File.dirname(__FILE__)
+
+config = YAML::load(ERB::new(File.read(File.join(CONFIG_DIR, "database.yml"))).result)
+
+Time.zone = "Tokyo"
+ActiveRecord::Base.establish_connection(config["development"])
+ActiveRecord::Base.time_zone_aware_attributes = true
+
+PROJECT_ROOT_DIR = File.expand_path("..", CONFIG_DIR)
+
+model_files = File.join(PROJECT_ROOT_DIR, "app", "models", "**", "*.rb")
+Dir.glob(model_files).map do |f|
+  require f
+end
